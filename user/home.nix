16d2f9703e7870c73c580bc0894be0e062315b4b
willow <hai@wlo.moe>
2025-05-29 23:09:33 +0100

    feat(user/home): specify cursor; specify prefer-dark

--- user/home.nix
+++ user/home.nix
@@ -1,12 +1,5 @@
+{ pkgs, ... }:
 {
-  programs.home-manager.enable = true;
-
-  home = {
-    username = "willow";
-    homeDirectory = "/home/willow";
-    stateVersion = "25.11";
-  };
-
   imports = [
     ./ctp.nix
     ./apps
@@ -14,6 +7,36 @@
     ./programs
     ./desktop
   ];
+  programs.home-manager.enable = true;
+
+  home = {
+    username = "willow";
+    homeDirectory = "/home/willow";
+    stateVersion = "25.11";
+    pointerCursor = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+      size = 24;
+      x11 = {
+        enable = true;
+        defaultCursor = "Adwaita";
+      };
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+  };
 
   xdg.enable = true;
   fonts.fontconfig.enable = true;
