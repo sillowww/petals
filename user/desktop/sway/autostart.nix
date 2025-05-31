c1ecb7da8f57dbf61b1786b21514d8411f22e906
willow <hai@wlo.moe>
2025-06-01 00:44:16 +0100

    feat(sway): remove waybar from autostart

--- user/desktop/sway/autostart.nix
+++ user/desktop/sway/autostart.nix
@@ -3,7 +3,6 @@
   wayland.windowManager.sway = {
     extraConfig = lib.mkAfter ''
       exec ${pkgs.swww}/bin/swww-daemon && _random-wallpaper
-      exec ${pkgs.waybar}/bin/waybar
       exec systemctl --user import-environment PATH && systemctl --user restart xdg-desktop-portal.service
 
       exec swaymsg "workspace 3; exec zeditor;"; assign [class="dev.zed.Zed"] 1
