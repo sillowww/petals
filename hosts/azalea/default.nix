3d08cd8c82dd44da2c4d952ec921eb883856de80
willow <hai@wlo.moe>
2025-06-04 13:39:09 +0100

    feat(hosts/azalea): add additional user

--- hosts/azalea/default.nix
+++ hosts/azalea/default.nix
@@ -30,6 +30,11 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWD6wk95qNhk/36vEH34qIRp/TPCcQ+D+u5Xd9/N0m1 hai@wlo.moe"
       ];
     };
+    users."meow" = {
+      isNormalUser = true;
+      shell = pkgs.fish;
+      hashedPassword = null;
+    };
     users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWD6wk95qNhk/36vEH34qIRp/TPCcQ+D+u5Xd9/N0m1 hai@wlo.moe"
     ];
