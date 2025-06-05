7594fe261352645443ac31c90bd5dbed056614a7
willow <hai@wlo.moe>
2025-06-05 13:26:09 +0100

    feat(hosts/azalea): enable caddy for pds

--- hosts/azalea/atproto-pds.nix
+++ hosts/azalea/atproto-pds.nix
@@ -1,3 +1,31 @@
+{ lib, ... }:
+let
+  mkPdsVhost = hostName: {
+    inherit hostName;
+    tls = "on_demand";
+    reverseProxy = {
+      to = "localhost:8090";
+    };
+    extraConfig = ''
+      tls {
+        on_demand
+      }
+      reverse_proxy localhost:8090
+    '';
+  };
+
+  pdsHostnames = [
+    "pds.wlo.moe"
+    "*.wlo.moe"
+  ];
+
+  pdsVirtualHosts = lib.listToAttrs (
+    map (hn: {
+      name = hn;
+      value = mkPdsVhost hn;
+    }) pdsHostnames
+  );
+in
 {
   services.pds = {
     enable = true;
@@ -10,5 +38,16 @@
       PDS_PORT = 8090;
       PDS_DATA_DIRECTORY = "/var/lib/pds";
     };
+  };
+
+  services.caddy = {
+    enable = true;
+    email = "hai@wlo.moe";
+    virtualHosts = pdsVirtualHosts;
+    extraConfig = ''
+        on_demand_tls {
+      		ask http://localhost:8090/tls-check
+       	}
+    '';
   };
 }
